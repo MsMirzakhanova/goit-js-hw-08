04a62990623fe2bf88af43455b5a788fec4a7567
MsMirzakhanova <98583249+MsMirzakhanova@users.noreply.github.com>
2022-10-21 13:17:32 +0300

    task 3 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+import Player from '@vimeo/player';  
+var throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+
+const onPlay = function(data) {
+    // data is an object containing properties specific to that event
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+
+player.setCurrentTime(savedTime).then(function (seconds) {
+const savedTime = localStorage.setItem(`videoplayer-current-time`,seconds)
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,47 @@
+import throttle from "lodash.throttle";
+
+const STORAGE_KEY = `feedback-form-state`;
+var formData = {};
+
+const refs = {
+    form: document.querySelector(`.feedback-form`),
+    input: document.querySelector(`.feedback-form input`),
+    textarea: document.querySelector(`.feedback-form textarea`),
+};
+
+refs.form.addEventListener(`submit`, onFormSubmit);
+refs.form.addEventListener(`input`, throttle(onTextareaInput, 500));
+
+
+populateTextarea();
+
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    event.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY );   
+};
+
+function onTextareaInput(event) {
+     formData[event.target.name] = event.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+    console.log(formData);
+    
+};
+
+function populateTextarea() {
+    const savedMessage = localStorage.getItem(STORAGE_KEY);
+    const parsedSavedMessage = JSON.parse(savedMessage);
+    if (parsedSavedMessage) {
+        refs.textarea.value = parsedSavedMessage.message;
+        refs.input.value = parsedSavedMessage.email;
+    }
+};
+
+
+
+
+
+
+
 
