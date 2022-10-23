cce01002125796c93d3eb5ad6ea8cc1bc11f598c
MsMirzakhanova <98583249+MsMirzakhanova@users.noreply.github.com>
2022-10-23 13:12:37 +0300

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,24 +5,11 @@ const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
 
-const onPlay = function(data) {
-    // data is an object containing properties specific to that event
-};
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
+function onPlay({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', seconds);
+}
 
-player.setCurrentTime(savedTime).then(function (seconds) {
-const savedTime = localStorage.setItem(`videoplayer-current-time`,seconds)
-    // seconds = the actual time that the player seeked to
-}).catch(function(error) {
-    switch (error.name) {
-        case 'RangeError':
-            // the time was less than 0 or greater than the video’s duration
-            break;
-
-        default:
-            // some other error occurred
-            break;
-    }
-});
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
