63bd36d50916584382ca74250326b857c3bbc5ca
MsMirzakhanova <98583249+MsMirzakhanova@users.noreply.github.com>
2022-10-24 10:56:55 +0300

    task 2 upgrade

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -11,5 +11,23 @@ player.on('timeupdate', throttle(onPlay, 1000));
 function onPlay({ seconds }) {
   localStorage.setItem('videoplayer-current-time', seconds);
 }
+   
+const savedMessage = localStorage.getItem('videoplayer-current-time');
+  if (savedMessage) {
+    console.log(savedMessage);
+};
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+player.setCurrentTime(savedMessage).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+  
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
